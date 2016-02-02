e6ba5d44d429d6057a7169f1b0ee269c3249d864
jubu <jurajburian@gmail.com>
2016-02-02 09:11:50 +0100

    wip - documentation

--- src/main/scala/jubu/mailer/Prop.scala
+++ src/main/scala/jubu/mailer/Prop.scala
@@ -26,14 +26,27 @@ trait Prop extends MailKeys {
 }
 
 
+/**
+	*
+	* @param host The SMTP server to connect to.
+	* @param port The SMTP server port to connect to, if the connect() method doesn't explicitly specify one. Defaults to 25.
+	*/
 case class SmtpAddress(host: String, port: Int = 25) extends Prop {
 	override def convert = Seq(SmtpHostKey -> host, SmtpPortKey -> port)
 }
 
+/**
+	*
+	* @param timeout Socket connection timeout value in milliseconds. Default is infinite timeout.
+	*/
 case class SmtpConnectionTimeout(timeout: Int) extends Prop {
 	override def convert = Seq(SmtpConnectionTimeoutKey -> timeout)
 }
 
+/**
+	*
+	* @param from Email address to use for SMTP MAIL command. This sets the envelope return address. Defaults to msg.getFrom() or InternetAddress.getLocalAddress().
+	*/
 case class SmtpFrom(from: String) extends Prop {
 	override def convert = Seq(SmtpFromKey -> from)
 }
@@ -42,6 +55,10 @@ case class SmtpTimeout(timeout: Int) extends Prop {
 	override def convert = Seq(SmtpTimeoutKey -> timeout)
 }
 
+/**
+	*
+	* @param protocol Specifies the default message transport protocol. The Session method getTransport() returns a Transport object that implements this protocol. By default the first Transport provider in the configuration files is returned.
+	*/
 case class TransportProtocol(protocol: String = "smtp") extends Prop {
 	override def convert = Seq(TransportProtocolKey -> protocol)
 }
